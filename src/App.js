a6cd9e3217632099c1c68990f26ba7a9d2060c30
Phan Đức Nghĩa <123baconsau6969@gmail.com>
2023-11-26 23:19:38 +0700

    add new login

--- src/App.js
+++ src/App.js
@@ -2,41 +2,40 @@ import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
 import { DefaultLayout } from '~/layouts';
-import AuthUI from '~/features/login/Auth';
+import CheckoutForm from '~/features/login/Auth';
 
 function App() {
     return (
-<<<<<<< Updated upstream:src/App.js
-        <Router>
-            <div className="App">
-                <Routes>
-                    {publicRoutes.map((route, index) => {
-                        const Page = route.component;
-                        let Layout = DefaultLayout;
+        // <Router>
+        //     <div className="App">
+        //         <Routes>
+        //             {publicRoutes.map((route, index) => {
+        //                 const Page = route.component;
+        //                 let Layout = DefaultLayout;
+        //
+        //                 if (route.layout) {
+        //                     Layout = route.layout;
+        //                 } else if (route.layout === null) {
+        //                     Layout = Fragment;
+        //                 }
+        //
+        //                 return (
+        //                     <Route
+        //                         key={index}
+        //                         path={route.path}
+        //                         element={
+        //                             <Layout>
+        //                                 <Page />
+        //                             </Layout>
+        //                         }
+        //                     />
+        //                 );
+        //             })}
+        //         </Routes>
+        //     </div>
+        // </Router>
 
-                        if (route.layout) {
-                            Layout = route.layout;
-                        } else if (route.layout === null) {
-                            Layout = Fragment;
-                        }
-
-                        return (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <Layout>
-                                        <Page />
-                                    </Layout>
-                                }
-                            />
-                        );
-                    })}
-                </Routes>
-            </div>
-        </Router>
-=======
-        <AuthUI />
+        < CheckoutForm />
         // <Router>
         //     <div className="flex flex-col min-h-full">
         //         <Routes>
@@ -65,7 +64,6 @@ function App() {
         //         </Routes>
         //     </div>
         // </Router>
->>>>>>> Stashed changes:src/app/App.js
     );
 }
 
