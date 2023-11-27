34629e36437085103e088329d78a6fdc0b0dabe0
baoha7204 <baoha3604@gmail.com>
2023-11-27 22:03:15 +0700

    remove cart slice from store

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import searchReducer from '~/features/search/searchSlice';
-import cartReducer from '~/features/cart/cartSlice';
 import searchingResultsReducer from '~/features/searchResults/searchResultsSlice';
 import spotlightReducer from '~/features/spotlight/spotlightSlice';
 
 export const store = configureStore({
     reducer: {
         search: searchReducer,
-        cart: cartReducer,
         searchResults: searchingResultsReducer,
         spotlight: spotlightReducer,
     },
